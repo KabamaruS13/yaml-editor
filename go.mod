7432ec7f62413fd567b1f804f422e8d7cf820794
s.giagkiozis <s.giagkiozis@thebeat.co>
2022-07-19 09:22:59 +0300

    work in progress, insert, update, delete actions added

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module yaml-editor
 
 go 1.18
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/stretchr/signature v0.0.0-20160104132143-168b2a1e1b56 // indirect
+	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
+	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/stretchr/stew/objects"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
@@ -14,19 +16,69 @@ func main() {
 
 	valueFile := os.Getenv("INPUT_VALUEFILE")
 	propertyPath := os.Getenv("INPUT_PROPERTYPATH")
+	key := os.Getenv("INPUT_KEY")
 	value := os.Getenv("INPUT_VALUE")
 	action := os.Getenv("INPUT_ACTION")
+
+	err := yamlEdit(valueFile, propertyPath, key, value, action)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+}
+
+func yamlEdit(valueFile, propertyPath, key, value, action string) error {
 
 	yamlFile, err := ioutil.ReadFile(valueFile)
 	if err != nil {
 		fmt.Printf("Error reading YAML file: %s\n", err)
+		return err
 	}
 
 	var yamlConfig map[string]interface{}
 	err = yaml.Unmarshal(yamlFile, &yamlConfig)
 	if err != nil {
 		fmt.Printf("Error parsing YAML file: %s\n", err)
+		return err
 	}
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s,%s,%s,%s,%s`, output, valueFile, propertyPath, value, action))
+	if action == "insert" {
+		obj := objects.Map(yamlConfig).Get(propertyPath)
+		objMap, ok := obj.(map[string]interface{})
+		if ok { // easy, just add another key-value pair
+			objMap[key] = value
+			objects.Map(yamlConfig).Set(propertyPath, objMap)
+		} else { // this is just a key-value pair, so I need to delete the value and make it an interface
+
+		}
+	} else if action == "update" {
+		objects.Map(yamlConfig).Set(propertyPath, value)
+	} else if action == "delete" {
+		if propertyPath != "" { // I need to access the parent map to delete the key
+			obj := objects.Map(yamlConfig).Get(propertyPath)
+			objMap := obj.(map[string]interface{})
+			delete(objMap, key)
+			objects.Map(yamlConfig).Set(propertyPath, objMap)
+		} else { // easy, key is on the root so I can delete it right away
+			delete(yamlConfig, key)
+		}
+	} else {
+		fmt.Printf("Wrong action selected: %s\n", action)
+		return errors.New("wrong action")
+	}
+
+	m, err := yaml.Marshal(yamlConfig)
+	if err != nil {
+		fmt.Printf("Error marshaling YAML file: %s\n", err)
+		return err
+	}
+
+	err = ioutil.WriteFile(valueFile, m, 0644)
+	if err != nil {
+		fmt.Printf("Error writing YAML file: %s\n", err)
+		return err
+	}
+
+	return nil
 }
